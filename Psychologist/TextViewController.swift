db44d5cae7c146b6467dcdab86781a48ddb9f295
ccossette <carlocossette@Carlo-Cossettes-MacBook-Pro.local>
2015-06-08 17:08:01 -0700

    Stanford Swift course demo of multiple MVC's using Split View Controllers

---
+++ Psychologist/TextViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TextViewController.swift
+//  Psychologist
+//
+//  Created by Carlo Cossette on 2015-06-02.
+//  Copyright (c) 2015 C Cossette. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    {
+        didSet
+        {
+            textView.text = text
+        }
+    }
+    
+    var text: String = ""
+    {
+        didSet
+        {
+            textView?.text = text
+        }
+    }
+    override var preferredContentSize: CGSize
+    {
+        get {
+            if textView != nil && presentingViewController != nil
+            {
+                return textView.sizeThatFits(presentingViewController!.view.bounds.size)
+            } else
+            {
+                return super.preferredContentSize 
+            }
+        }
+        set { super.preferredContentSize = newValue }
+    }
+}
